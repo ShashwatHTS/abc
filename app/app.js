d6b03c744e9848bf70b8471e890e5311cef70500
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-08-26 14:22:20 +0530

    usercontroller initated

--- app/app.js
+++ app/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dbConnect from "../config/dbConnect.js";
+import userRoutes from "../routes/user.route.js";
 
 // db connect
 dbConnect();
 const app = express()
 
+
+// routes
+app.use('/',userRoutes)
 export default app;
